d699deda7a7b2d9ed987b366bc0aaccc0b6f572b
SunilNeupane77 <sunil.799406@sdc.tu.edu.np>
2024-04-17 10:43:14 +0545

    file updated

--- my-app/src/schemas/acceptMessageSchema.ts
+++ my-app/src/schemas/acceptMessageSchema.ts
@@ -1,4 +1,4 @@
-import { z } from "zod"
+import { z } from "zod";
 export const AcceptMessageSchema=z.object({
    acceptMessage:z.string(),
-})
+});
--- my-app/src/schemas/signUpSchema.ts
+++ my-app/src/schemas/signUpSchema.ts
@@ -7,5 +7,6 @@ export const usernameValidation=z
 
 export const signUpSchema=z.object({
 username: usernameValidation,
-email:z.string().email()
+email:z.string().email({message:"Invalid email address"}),
+password:z.string().min(6,"password must be at least 6 characters")
 });
